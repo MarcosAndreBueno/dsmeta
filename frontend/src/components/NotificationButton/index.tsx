a6d954c277b1c1cb6237696d2d947e2195c4f668
MarcosAndreBueno <www.bueno@yahoo.com>
2022-11-19 18:22:21 -0300

    Send notification

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,10 +1,25 @@
+import axios from 'axios';
 import icon from '../../assets/img/notification-icon.svg';
+import { BASE_URL } from '../../utils/request';
 import './styles.css';
 
-function NotificationButton() {
+//props permite que componente react receba um parâmetro, no caso Id
+type Props = {
+  saleId: number;
+}
+
+//ao clicar envia notificação por sms
+function handleClick(saleId: number) {
+  axios(`${BASE_URL}/sales/${saleId}/notification`)
+  .then(response => {
+      console.log("SUCESSO")
+  });
+}
+
+function NotificationButton({ saleId }: Props) {
   return (
     //className em react é o class do html
-    <div className="dsmeta-red-btn">
+    <div className="dsmeta-red-btn" onClick={() => handleClick(saleId)}>
       <img src={icon} alt="Notificar" />
     </div>
   )
